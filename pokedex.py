532c6481c98a99e52597b14a8037b77227107cb3
SatyamAK <satyam.ak99@gmail.com>
2024-03-25 13:10:14 +0530

    refactor. moving all functions to module

--- pokedex.py
+++ pokedex.py
@@ -3,22 +3,23 @@ from requests import get
 from pokemonDataExtractor import pokemon_data_extractor
 from json import dump
 
-url = 'https://pokemondb.net/pokedex/national'
-pageHtml = BeautifulSoup(get(url).text, "lxml")
+def pokedex_populator():
+    url = 'https://pokemondb.net/pokedex/national'
+    pageHtml = BeautifulSoup(get(url).text, "lxml")
 
-generations = [ generation.text.strip().replace(' Pokémon', '') for generation in pageHtml.find_all('h2') ]
+    generations = [ generation.text.strip().replace(' Pokémon', '') for generation in pageHtml.find_all('h2') ]
 
-pokedex = {}
+    pokedex = {}
 
-pokemon_data = pageHtml.find_all('div', class_='infocard-list infocard-list-pkmn-lg')
+    pokemon_data = pageHtml.find_all('div', class_='infocard-list infocard-list-pkmn-lg')
 
-pokemons = []
-for pokemons_each_generation in pokemon_data:
-    pokemon_data_generation_wise =  [pokemon_data_extractor(data.find('span', class_="infocard-lg-data text-muted")) for data in pokemons_each_generation.find_all('div', class_='infocard')]
-    pokemons.append(pokemon_data_generation_wise)
+    pokemons = []
+    for pokemons_each_generation in pokemon_data:
+        pokemon_data_generation_wise =  [pokemon_data_extractor(data.find('span', class_="infocard-lg-data text-muted")) for data in pokemons_each_generation.find_all('div', class_='infocard')]
+        pokemons.append(pokemon_data_generation_wise)
 
-for index, generation in enumerate(generations):
-    pokedex[generation] = pokemons[index]
+    for index, generation in enumerate(generations):
+        pokedex[generation] = pokemons[index]
 
-with open('data/pokedex.json', 'w') as fp:   
-    dump(pokedex, fp)
+    with open('data/pokedex.json', 'w') as fp:   
+        dump(pokedex, fp)
--- pokemonGameWisePokedex.py
+++ pokemonGameWisePokedex.py
@@ -5,32 +5,41 @@ from json import dump
 from pokemonDataExtractor import pokemon_data_extractor
 
 games_list_url = 'https://pokemondb.net/pokedex'
-gamesPageHtml = BeautifulSoup(get(games_list_url).text, 'lxml')
-games = [ game.find('a').text.strip() for game in gamesPageHtml.find('nav', class_='panel panel-nav').find_all('ul')[1].find_all('li') ]
-games.pop(0)
 
-base_url = games_list_url+'/game'
-games_url = []
+def _games_list_extractor():
+    gamesPageHtml = BeautifulSoup(get(games_list_url).text, 'lxml')
+    games = [ game.find('a').text.strip() for game in gamesPageHtml.find('nav', class_='panel panel-nav').find_all('ul')[1].find_all('li') ]
+    games.pop(0)
+    return games
 
-def game_name_processor(game_name):
+games = _games_list_extractor()
+
+def _game_name_processor(game_name):
     return sub(r'[^a-zA-Z0-9\s]+', '', game_name).lower().replace('  ', ' ').replace(' ', '-')
 
-for game in games:
-    if(game == 'Black 2 & White 2'):
-        game = 'Black & White 2'
-    if(game == "Let's Go Pikachu & Let's Go Eevee"):
-        game = "Let's Go Pikachu & Eevee"
+def _games_url_extractor():
+    base_url = games_list_url+'/game'
+    games_url = []
+    for game in games:
+        if(game == 'Black 2 & White 2'):
+            game = 'Black & White 2'
+        if(game == "Let's Go Pikachu & Let's Go Eevee"):
+            game = "Let's Go Pikachu & Eevee"
+        game_url = base_url+"/"+_game_name_processor(game)
+        games_url.append(game_url)
+        
+    return games_url
 
-    game_url = base_url+"/"+game_name_processor(game)
-    games_url.append(game_url)
+games_url = _games_url_extractor()
 
-for index, game_url in enumerate(games_url):
-    dex = {}
-    pokemonListPage = BeautifulSoup(get(game_url).text, 'lxml')
-    pokemon_data = pokemonListPage.find('div', class_='infocard-list infocard-list-pkmn-lg')
-    pokemons = [ pokemon_data_extractor(data) for data in pokemon_data.find_all('span', class_='infocard-lg-data text-muted') ]
-    dex['game'] = games[index]
-    dex['pokemon'] = pokemons
-    file_path = "data/"+game_name_processor(games[index])+'.json'
-    with open(file_path, 'w') as fp:   
-        dump(dex, fp)
+def pokemon_game_wise_data_populator():
+    for index, game_url in enumerate(games_url):
+        dex = {}
+        pokemonListPage = BeautifulSoup(get(game_url).text, 'lxml')
+        pokemon_data = pokemonListPage.find('div', class_='infocard-list infocard-list-pkmn-lg')
+        pokemons = [ pokemon_data_extractor(data) for data in pokemon_data.find_all('span', class_='infocard-lg-data text-muted') ]
+        dex['game'] = games[index]
+        dex['pokemon'] = pokemons
+        file_path = "data/"+_game_name_processor(games[index])+'.json'
+        with open(file_path, 'w') as fp:   
+            dump(dex, fp)
